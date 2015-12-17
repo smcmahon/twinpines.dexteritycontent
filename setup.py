da2fc5001e46af54dd51770e598ffffb1f2e462f
Steve McMahon <steve@dcn.org>
2015-12-16 16:40:27 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='twinpines.dexteritycontent',
+      version=version,
+      description="Twin Pines custom types",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='steve@dcn.org',
+      url='https://github.com/smcmahon',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['twinpines'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity [relations]',
+          'plone.app.relationfield',
+          'plone.namedfile [blobs]',
+          'collective.maplocbehavior',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
